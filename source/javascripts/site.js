9bf0ea17addcceb1eac68b9733fdc9772debae28
Rashad DuPaty <radupaty@gmail.com>
2024-06-03 12:55:36 +0900

    working on gallery section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -23,4 +23,25 @@ function openMenu(evt, menuName) {
 // Get the element with id="defaultOpen" and click on it
 window.addEventListener("DOMContentLoaded", function() {
   document.getElementById("defaultOpen").click();
+
+  // Gallery Section
+  let scrollContainer = document.querySelector(".gallery");
+  let backBtn = document.getElementById("backBtn");
+  let nextBtn = document.getElementById("nextBtn");
+
+  scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+  });
+
+  nextBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+  });
+
+  backBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+  });
 });
