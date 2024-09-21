2345d1fc6a22622f847f31451a9d51775a2c4b55
Rashad DuPaty <radupaty@gmail.com>
2024-09-21 15:02:50 +0900

    ani2

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Set up a timeline for automatic switching
-  let tl = gsap.timeline({ repeat: -1, repeatDelay: 7 });
+  let tl = gsap.timeline({ repeat: -1, repeatDelay: 10 });
 
   previewImgs.forEach((img, index) => {
     tl.to(mainImg, {
