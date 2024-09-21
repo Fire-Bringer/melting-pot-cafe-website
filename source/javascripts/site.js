ec15c21ec578ed89f55721f84143354493826f2f
Rashad DuPaty <radupaty@gmail.com>
2024-09-21 09:19:17 +0900

    extended switch delay

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Set up a timeline for automatic switching
-  let tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+  let tl = gsap.timeline({ repeat: -1, repeatDelay: 3 });
 
   previewImgs.forEach((img, index) => {
     tl.to(mainImg, {
